092c02653dce31b0761e71baf7eff6ba4d423e2f
BobaeLee <dngnpdl@naver.com>
2015-06-30 11:31:45 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello!");
+		
+		Thread.sleep(10000); // 10s sleep
+		
+		System.out.println("Hello again!");
 	}
 
 }
